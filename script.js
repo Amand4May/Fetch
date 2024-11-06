80bc4b69b73b782e798e3d713228edc14dd1a3e5
Amand4May <ahtaldananda18@gmail.com>
2024-11-05 21:55:48 -0300

    ultimavez

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
-const URL = 'https://672a8935976a834dd023b47e.mockapi.io/usuario/:cliente';
+const URL = 'https://672a8935976a834dd023b47e.mockapi.io/usuario/animal';
 
-async function chamarApi(){
-    const resp = await fetch(URL);
-    console.log(resp);
+function buscaAnimal(){
+    
+    fetch(URL)
+        .then(response => response.json())
+        .then(resp => {
+            console.log(resp);
+            atualiza(resp);
+        });
 }
 
-chamarApi();
+function atualiza(lista){
+    let listaAnimal = document.querySelector('#lista');
+
+    listaAnimal.innerHTML = ' ';
+
+    lista.forEach((animal, index) => {
+        listaAnimal.innerHTML +=
+        `<li id="${index}" class="">${animal.id} - <b>${animal.nome}<b> - (${animal.idade}) - ${animal.raca}</li>`;
+    });
+
+}
+
+function cadastro(){
+
+    fetch(URL,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
+            nome: 'Totó',
+            idade: 12,
+            raca: 'Cachorro'
+        })
+    })
+    .then(response => response.json())
+    .then(novo => {
+        console.log(novo);
+        buscaAnimal();
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
